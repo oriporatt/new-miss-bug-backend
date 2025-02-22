de26e79cb51a9dc09f46bf00c8c0b3fe13a329c1
oriporatt <oriporatt@gmail.com>
2025-02-22 08:04:13 +0200

    uodated

--- config/prod.js
+++ config/prod.js
@@ -1,5 +1,5 @@
 
 export default {
-    dbURL: process.env.MONGO_URL || 'mongodb+srv://anistu:WGvRd3jFNR8tzi3w@cluster0.uqvtq.mongodb.net/',
+    dbURL: process.env.MONGO_URL || 'mongodb+srv://oriporatt:OoWE7DZcmSUIL1gT@cluster0.84sjc.mongodb.net/',
     dbName: process.env.DB_NAME || 'car_db'
 }

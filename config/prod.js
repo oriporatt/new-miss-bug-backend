a8c8c9bd9dc5a647f280452ab141492e76a66859
oriporatt <oriporatt@gmail.com>
2025-02-23 20:46:16 +0200

    Working on car DB local  and remote. starting move to BUG

--- config/prod.js
+++ config/prod.js
@@ -1,5 +1,5 @@
 
 export default {
-    dbURL: process.env.MONGO_URL || 'mongodb+srv://oriporatt:OoWE7DZcmSUIL1gT@cluster0.84sjc.mongodb.net/',
+    dbURL: process.env.MONGO_URL || 'mongodb+srv://oriporatt:LqXDE6dQrsUdF7Ru@cluster0.84sjc.mongodb.net/',
     dbName: process.env.DB_NAME || 'car_db'
 }
